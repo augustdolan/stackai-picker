e542ec27f92e7c2c4d182bc5a0ee04e09add1453
August Dolan <august.dolan1@gmail.com>
2024-10-29 11:56:07 -0400

    Add counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
-import Image from "next/image";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
   return (
     <main className="p-8">
-      <div className="flex pb-4 border-b gap-2 mb-4">
+      <div className="flex pb-4 border-b gap-2 mb-8">
         <h1 className="font-bold text-lg">Google Drive</h1>
         <div className="bg-secondary px-2 text-lg rounded">Beta</div>
       </div>
-      <div>testweoifjweoifj</div>
+      <div className="flex justify-between border-b pb-1">
+        <div className="flex gap-2">
+          <Checkbox id="select-all" />
+          <Label htmlFor="select-all">Select all</Label>
+        </div>
+        <div className="text-zinc-400">{count}</div>
+      </div>
     </main>
   );
 }
