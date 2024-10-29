2d8605a79a8579572ab4234714ef3e7382cafc02
August Dolan <august.dolan1@gmail.com>
2024-10-29 11:23:21 -0400

    migrate from app to pages

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-8">
+      <div className="flex pb-4 border-b gap-2 mb-4">
+        <h1 className="font-bold text-lg">Google Drive</h1>
+        <div className="bg-secondary px-2 text-lg rounded">Beta</div>
+      </div>
+      <div>testweoifjweoifj</div>
+    </main>
+  );
+}
