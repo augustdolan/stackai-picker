d7659c4f113ff03409928e495f35204194b1ffbd
August Dolan <august.dolan1@gmail.com>
2024-11-02 13:20:42 -0400

    remove auth caching for security, anticipate route caching

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -1,9 +1,8 @@
 import { auth } from "@/auth";
-export async function stackAiFetch(slug: string, config?: RequestInit) {
+export async function stackAiFetch<T>(slug: string, config?: RequestInit) {
   const session = await auth();
   const response = await fetch(`${process.env.STACKAI_BACKEND_URL}/${slug}`, {
     ...config,
-    cache: "force-cache", // cache even when authorization headers are present
     next: {
       revalidate: 3600, // generally, refetch all calls at most every hour
     },
@@ -14,13 +13,7 @@ export async function stackAiFetch(slug: string, config?: RequestInit) {
       "Content-Type": "application/json",
     }
   })
-  const parsed = await response.json();
+  const parsed: T = await response.json();
   return parsed;
 };
 
-// everything is cached, so recalling this is no harm
-// Also, obviously this could be extendeed to support multiple connections
-export async function getConnection() {
-  const connections = await stackAiFetch("connections?connection_provider=gdrive&limit1")
-  return connections;
-};
