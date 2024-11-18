655f6f9ed37bb4be377d8c25f0e6483c1648c967
August Dolan <august.dolan1@gmail.com>
2024-11-18 15:11:02 -0500

    Show more page when first loading

--- app/connections/[connectionId]/page.tsx
+++ app/connections/[connectionId]/page.tsx
@@ -1,6 +1,8 @@
 import ConnectionResourceList from "@/components/connections/ConnectionResourceList";
 import RootDirectory from "@/components/connections/RootDirectory";
 import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 
 export default async function Connection({ params }: { params: Promise<{ connectionId: string }> }) {
   // assuming the backend normalizes different connection representations
@@ -9,7 +11,9 @@ export default async function Connection({ params }: { params: Promise<{ connect
     <main className="p-8 h-full flex flex-col justify-center">
       <Card className="relative p-8 h-2/3">
         <ConnectionResourceList>
-          <RootDirectory connectionId={connectionId} />
+          <Suspense fallback={<Skeleton className="h-4 w-50% m-2" />}>
+            <RootDirectory connectionId={connectionId} />
+          </Suspense>
         </ConnectionResourceList>
       </Card>
     </ main>
